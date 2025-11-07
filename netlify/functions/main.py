36e22c6a1481c3a817060768042a2c47b13f38b6
Denys Demenko <d.demenko@foodtech.team>
2025-11-07 16:17:23 +0400

    api

--- netlify/functions/main.py
+++ netlify/functions/main.py
@@ -1,6 +1,4 @@
-from app import mcp_app
+from main import http_app
 from mangum import Mangum
 
-# Get the SSE Starlette app from MCP
-sse_app = mcp_app.sse_app()
-handler = Mangum(sse_app)
+handler = Mangum(http_app)
